10668205a7c73fd58a0ef01320039a68b15b3180
Kasper <kasper4131@gmail.com>
2018-03-02 18:54:20 +0100

    I have not coded in a long time so I honesly hav no ide whats changed.

--- klib/exceptions.h
+++ klib/exceptions.h
@@ -1,9 +1,8 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
 #pragma once
-#include <string>
-#include <iostream>
-
-
 #define SUCCESS              "<::SUCCESS::>"
-#define STD_CALL_ERROR       ":CALL::ERROR" //":::::::::CALL::::::::::::::ERR:::::::::"
-#define VALUE_DOES_NOT_EXIST "VALUE::DOES::NOT::EXIST:" //"::::____VA:LU:E__:DO:E:S:::NO:T::EX:IS::T::::::::::"
-#define INVALID_VALUE        "INVALID::VALUE::" //"::::::::INVALID:::::::::VAL::::UE:::"
+#define STD_CALL_ERROR       ":CALL::ERROR"
+#define VALUE_DOES_NOT_EXIST "VALUE::DOES::NOT::EXIST:"
+#define INVALID_VALUE        "INVALID::VALUE::"
+#endif // EXCEPTIONS_H
