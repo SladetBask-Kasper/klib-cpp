5b6511558407d3afc1fe5d78d7fceeaa65587308
Kasper <kasper4131@gmail.com>
2021-01-04 04:55:39 +0100

    Delete exceptions.h
    This file seems obsolete

--- klib/exceptions.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXCEPTIONS_H
-#define EXCEPTIONS_H
-#pragma once
-#define SUCCESS              "<::SUCCESS::>"
-#define STD_CALL_ERROR       ":CALL::ERROR"
-#define VALUE_DOES_NOT_EXIST "VALUE::DOES::NOT::EXIST:"
-#define INVALID_VALUE        "INVALID::VALUE::"
-#endif // EXCEPTIONS_H
